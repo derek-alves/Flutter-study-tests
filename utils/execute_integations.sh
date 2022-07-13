27de1ae838c3f92505172c8891006c6ced1ee4c9
derek-alves <derek.alves1998@hotmail.com>
2022-07-12 21:23:38 -0300

    step: run two tests in script

--- utils/execute_integations.sh
+++ utils/execute_integations.sh
@@ -1,5 +1,7 @@
-flutter drive \
-  --driver=C:/Users/usemobile/Documents/projects/tests/test_driver/integration_test_driver.dart \
-  --target=C:/Users/usemobile/Documents/projects/tests/integration_test/login_test.dart
+fvm flutter drive \
+  --driver=C:/Users/derek/Documents/projects/Flutter-study-tests/test_driver/integration_test_driver.dart \
+  --target=C:/Users/derek/Documents/projects/Flutter-study-tests/integration_test/login_test.dart
 
-   
+fvm flutter drive \
+  --driver=C:/Users/derek/Documents/projects/Flutter-study-tests/test_driver/integration_test_driver.dart \
+  --target=C:/Users/derek/Documents/projects/Flutter-study-tests/integration_test/multiple_test.dart
